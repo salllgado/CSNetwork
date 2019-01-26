64fc600d3ee08ddf13077616b4b8f1e086b66e63
Chrystian <salllgado@hotmail.com.br>
2019-01-26 18:41:06 -0200

    #Comment criando testes

--- Example/CSNetwork_ExampleTests/CSNetwork_ExampleTests.swift
+++ Example/CSNetwork_ExampleTests/CSNetwork_ExampleTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+@testable import CSNetwork_Example
+@testable import CSNetwork
 
 class CSNetwork_ExampleTests: XCTestCase {
 
@@ -19,15 +21,12 @@ class CSNetwork_ExampleTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let objResponse = BasicRequest(apikey: "111", name: "AAA")
+        
+        do {
+            
+        } catch {
+            XCTFail("\(error.localizedDescription)")
         }
     }
-
 }
